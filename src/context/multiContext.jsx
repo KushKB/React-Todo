04a2fcd35e8b9db869b2e0cdeb9636c0d978d633
Kush Bhatia <kushbhatia987@gmail.com>
2024-01-31 23:20:46 +0530

    First

---
+++ src/context/multiContext.jsx
@@ -0,0 +1,18 @@
+// import React from "react";
+// import { useState } from "react";
+// import { createContext } from "react";
+
+// export const multiContext=createContext();
+
+// // export const multiprovider=multi.Provider;
+
+// const multiContextprovider=({children})=>{
+//     const [field,setfield]=useState(null);
+//     return(
+//         <multiContext.Provider value={{field,setfield}}>
+//             {children}
+//         </multiContext.Provider>
+//     )
+// }
+
+// export default multiContextprovider;
